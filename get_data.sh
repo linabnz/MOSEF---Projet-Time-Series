73c214db669de1ac2d1307e172e1d49efa738374
Lina Benzemma <lbenzemma@univ-paris1.fr>
2025-02-04 12:00:04 +0100

    lina

--- get_data.sh
+++ get_data.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-if [ ! -f "data/traffic.csv" ]; then
-    echo "Téléchargement des données depuis opendata.paris.fr..."
-    
-    curl -X 'GET' \
-        'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/comptages-routiers-permanents/exports/csv?delimiter=%2C&list_separator=%2C&quote_all=false&with_bom=true&where=libelle%20like%20%22AE_A13_bretelle_5%22' \
-        -H 'accept: */*' -o data/traffic.csv
 
-    echo "Téléchargement terminé."
-else
-    echo "Le fichier data/traffic.csv existe déjà. Pas de téléchargement nécessaire."
-fi
+
+mkdir -p data
+datasets=("AE_A13_bretelle_5" "AI_A1_bretelle_8" "Alesia" "Amsterdam" "Auber")
+for label in "${datasets[@]}"
+do
+    file="data/${label}.csv"
+
+    if [ ! -f "$file" ]; then
+        echo "Téléchargement des données pour $label depuis opendata.paris.fr..."
+        
+        curl -X 'GET' \
+            "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/comptages-routiers-permanents/exports/csv?delimiter=%2C&list_separator=%2C&quote_all=false&with_bom=true&where=libelle%20like%20%22${label// /%20}%22" \
+            -H 'accept: */*' -o "$file"
+
+        echo "Téléchargement terminé pour $label."
+    else
+        echo "Le fichier $file existe déjà. Pas de téléchargement nécessaire."
+    fi
+done
+
